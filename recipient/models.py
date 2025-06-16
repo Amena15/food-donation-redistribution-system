b95eac2a9997ce36b824c0dd05ad9030b725f6d4
Farah Hanim <farahhanim8727@icloud.com>
2025-06-16 22:21:12 +0800

    Recipient Model

--- recipient/models.py
+++ recipient/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from fooddonor.models import Profile, FoodDonation
+from django.contrib.auth.models import User
 
 class RecipientPreference(models.Model):
     recipient = models.OneToOneField(Profile, on_delete=models.CASCADE, limit_choices_to={'role': 'recipient'})
@@ -9,3 +10,13 @@ class RecipientPreference(models.Model):
     
     def __str__(self):
         return f"Preferences for {self.recipient.user.username}"
+    
+
+class Recipient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20)
+    location = models.CharField(max_length=255)
+    needs_description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
